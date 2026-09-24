3c5f64f7f3c04b38ba8740344b1adbec633efbb1
Murilo Gomes <murilovasc.gomes@gmail.com>
2026-09-24 18:46:59 -0300

    feat: Adiciona atributos Produto

---
+++ System/src/Produto.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+
+//BigDecimal é um tipo espesifico para valor monetário.
+
+public class Produto {
+    private String nome;
+    private BigDecimal preco;
+    private float tamanho;
+    private int id;
+    private boolean ativo;
+}
